f83c97e2aadd9f1cd0256cd2ae4b123b79d805fe
Felipe Prestes Aranalde <felipe.aranalde@gmail.com>
2025-08-18 00:33:03 -0300

    Fix: proc-v7.S

--- kernel/bounds.c
+++ kernel/bounds.c
@@ -1,4 +1,5 @@
 #include <linux/postmarketos-compat.h>
+#include <linux/postmarketos-compat.h>
 /*
  * Generate definitions needed by the preprocessor.
  * This code generates raw asm output which is post-processed
