472b10ec403fd2b1d4a5dc42eb04d91c32e9d8e1
Felipe Prestes Aranalde <felipe.aranalde@gmail.com>
2025-08-27 22:19:58 -0300

    Update timex.h

--- arch/arm/include/asm/timex.h
+++ arch/arm/include/asm/timex.h
@@ -12,11 +12,8 @@
 #ifndef _ASMARM_TIMEX_H
 #define _ASMARM_TIMEX_H
 
-#ifdef CONFIG_ARCH_MSM
-#define CLOCK_TICK_RATE		1000000
-#else
-#include <mach/timex.h>
-#endif
+/* MSM8226 Timer - Direct definition to avoid mach/timex.h dependency */
+#define CLOCK_TICK_RATE 1000000
 
 typedef unsigned long cycles_t;
 #define get_cycles()	({ cycles_t c; read_current_timer(&c) ? 0 : c; })
